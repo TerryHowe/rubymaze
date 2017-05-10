95413620a21257bb2d55c150258e518c9e6a74d4
Terry Howe <terrylhowe@gmail.com>
2017-05-10 11:15:45 -0600

    rename rooms controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'maze/index'
 
+  resources :rooms
+  resources :passages
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
