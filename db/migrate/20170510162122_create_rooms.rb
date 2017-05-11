80d27f04308fee5d0407b52686d76d3a1d3561fd
Terry Howe <terrylhowe@gmail.com>
2017-05-11 10:13:46 -0600

    make basic maze in migration

--- db/migrate/20170510162122_create_rooms.rb
+++ db/migrate/20170510162122_create_rooms.rb
@@ -7,8 +7,5 @@ class CreateRooms < ActiveRecord::Migration[5.1]
       t.timestamps
     end
 	 add_index :rooms, [:x, :y], unique: true
-    Room.create(:x=>0,:y=>0);
-    Room.create(:x=>1,:y=>0);
-    Room.create(:x=>0,:y=>1);
   end
 end
--- db/migrate/20170510163551_create_passages.rb
+++ db/migrate/20170510163551_create_passages.rb
@@ -8,5 +8,12 @@ class CreatePassages < ActiveRecord::Migration[5.1]
       t.timestamps
     end
 	 add_index :passages, [:source, :direction], unique: true
+    r00 = Room.create(:x=>0,:y=>0);
+    r10 = Room.create(:x=>1,:y=>0);
+    r01 = Room.create(:x=>0,:y=>1);
+    Passage.create(:source=>r00, :destination=>r10, :direction=>0)
+    Passage.create(:source=>r10, :destination=>r00, :direction=>2)
+    Passage.create(:source=>r00, :destination=>r01, :direction=>1)
+    Passage.create(:source=>r01, :destination=>r00, :direction=>3)
   end
 end
