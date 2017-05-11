3ac0e91165d123b5f70f9cff5897877abe13c8b8
Terry Howe <terrylhowe@gmail.com>
2017-05-11 12:33:27 -0600

    start adding tests

--- app/models/passage.rb
+++ app/models/passage.rb
@@ -3,4 +3,8 @@ class Passage < ApplicationRecord
    belongs_to :destination, class_name: 'Room'
    enum direction: [:north, :east, :south, :west]
    validates :direction, uniqueness: {scope: :source, message: ' room passage must be unique'}
+
+   def get_direction()
+      return self.direction.to_s.capitalize
+   end
 end
--- test/models/passage_test.rb
+++ test/models/passage_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PassageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get direction" do
+    passage = Passage.new
+    passage.direction = :north
+    assert_equal("North", passage.get_direction())
+    passage.direction = :east
+    assert_equal("East", passage.get_direction())
+    passage.direction = :south
+    assert_equal("South", passage.get_direction())
+    passage.direction = :west
+    assert_equal("West", passage.get_direction())
+  end
 end
