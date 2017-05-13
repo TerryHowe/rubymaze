373b14199e682f4b58351f72f33543bf4cecb5d6
Terry Howe <terrylhowe@gmail.com>
2017-05-13 07:21:41 -0600

    add some model tests

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,14 @@ class Room < ApplicationRecord
    validates :x, numericality: {only_integer: true}
    validates :y, numericality: {only_integer: true}
    validates :x, uniqueness: {scope: :y, message: ' and Y must be unique'}
+   has_many :passages
+
+   def go_forward(direction)
+      return self.passages.first
+      passage = self.passages.select {|p| p.direction == direction}.first
+      if passage
+         return passage.destination
+      end
+      return nil
+   end
 end
--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "go foward" do
+    passage = Passage.new
+    destination = Room.new
+    destination.id = 1
+    destination.x = 0
+    destination.y = 1
+    source = Room.new
+    source.id = 0
+    source.x = 0
+    source.y = 0
+    passage.source = source
+    passage.destination = destination
+    passage.direction = :north
+    source.passages = [passage]
+
+    assert_equal(destination, source.go_forward('N'))
+  end
 end
