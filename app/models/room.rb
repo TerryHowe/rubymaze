f24fa777297e425ebf182d9f21059183efa81574
Terry Howe <terrylhowe@gmail.com>
2017-05-13 09:34:32 -0600

    get room test working

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,13 +2,14 @@ class Room < ApplicationRecord
    validates :x, numericality: {only_integer: true}
    validates :y, numericality: {only_integer: true}
    validates :x, uniqueness: {scope: :y, message: ' and Y must be unique'}
-   has_many :passages
+   has_many :passages, foreign_key: :source_id
 
    def go_forward(direction)
-      return self.passages.first
-      passage = self.passages.select {|p| p.direction == direction}.first
-      if passage
-         return passage.destination
+      direction = direction.to_s
+      for p in self.passages
+         if p.direction == direction
+            return p.destination
+         end
       end
       return nil
    end
--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -2,20 +2,8 @@ require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
   test "go foward" do
-    passage = Passage.new
-    destination = Room.new
-    destination.id = 1
-    destination.x = 0
-    destination.y = 1
-    source = Room.new
-    source.id = 0
-    source.x = 0
-    source.y = 0
-    passage.source = source
-    passage.destination = destination
-    passage.direction = :north
-    source.passages = [passage]
-
-    assert_equal(destination, source.go_forward('N'))
+    source = Room.where(x:0,y:0).first
+    destination = Room.where(x:0,y:1).first
+    assert_equal(destination, source.go_forward(:north))
   end
 end
