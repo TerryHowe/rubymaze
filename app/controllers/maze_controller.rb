d6b5ad3cafb024878bacb90557ef9e8d91a3e889
Terry Howe <terrylhowe@gmail.com>
2017-05-15 16:20:50 -0600

    rename show to maze for the controller

--- app/controllers/maze_controller.rb
+++ app/controllers/maze_controller.rb
@@ -1,5 +1,5 @@
 class MazeController < ApplicationController
-  def show
+  def maze
     @direction = params[:direction]
     @room = Room.where(x: params[:x].to_i, y: params[:y].to_i).first
   end
