f5026cc9697b6c9a6d53ec826dba44caf4736230
Terry Howe <terrylhowe@gmail.com>
2017-05-15 16:31:11 -0600

    add basic controller test

--- test/controllers/maze_controller_test.rb
+++ test/controllers/maze_controller_test.rb
@@ -1,9 +1,38 @@
 require 'test_helper'
 
 class MazeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get maze_index_url
+EXPECTED = """
+
+ \\                               
+  \\                              
+   +                             
+   |                             
+   |                             
+   |                             
+   |                             
+   |                             
+   |                             
+   |                             
+   |                             
+   |                             
+   |                             
+   |                             
+   |                             
+   |                             
+   +                             
+  /                              
+ /                               
+
+"""
+  test "should get maze" do
+    get maze_path(x: 0, y: 0, direction: 'N')
     assert_response :success
+    assert_equal "maze", @controller.action_name
+    assert_select "pre", EXPECTED
   end
+
+  #test "the maze helper" do
+  #  assert_equal 'abc', @controller.render(0,0,'N')
+  #end
 
 end
