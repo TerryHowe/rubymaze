0974f19dbb932270ce597e22e536d2785505e393
Terry Howe <terrylhowe@gmail.com>
2017-05-15 16:20:22 -0600

    clean up maze routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'maze/index'
-
+  get '/maze/:x/:y/:direction', to: 'maze#maze', as: 'maze'
   resources :rooms
   resources :passages
-  get '/maze/:x/:y/:direction', to: 'maze#show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
