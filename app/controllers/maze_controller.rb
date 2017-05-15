3f23d02129d5fa8fc031101c0147a40d743c159e
Terry Howe <terrylhowe@gmail.com>
2017-05-15 12:31:34 -0600

    add maze view and route

--- app/controllers/maze_controller.rb
+++ app/controllers/maze_controller.rb
@@ -1,4 +1,6 @@
 class MazeController < ApplicationController
-  def index
+  def show
+    @direction = params[:direction]
+    @room = Room.where(x: params[:x].to_i, y: params[:y].to_i).first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :rooms
   resources :passages
+  get '/maze/:x/:y/:direction', to: 'maze#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
