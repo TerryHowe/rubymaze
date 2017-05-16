f0f5b4a5948789d1fd945b1e0ba4a754a5d375ab
Terry Howe <terrylhowe@gmail.com>
2017-05-16 08:50:34 -0600

    update passage migration

--- db/migrate/20170510163551_create_passages.rb
+++ db/migrate/20170510163551_create_passages.rb
@@ -7,7 +7,7 @@ class CreatePassages < ActiveRecord::Migration[5.1]
 
       t.timestamps
     end
-	 add_index :passages, [:source, :direction], unique: true
+	 add_index :passages, [:source_id, :direction], unique: true
     r00 = Room.create(:x=>0,:y=>0);
     r10 = Room.create(:x=>1,:y=>0);
     r01 = Room.create(:x=>0,:y=>1);
