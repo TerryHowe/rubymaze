02f81c7f784538c4b0bba24e7cb9ab286e2b3fc9
Terry Howe <terrylhowe@gmail.com>
2017-05-15 18:29:32 -0600

    fix the view

--- app/controllers/maze_controller.rb
+++ app/controllers/maze_controller.rb
@@ -2,5 +2,7 @@ class MazeController < ApplicationController
   def maze
     @direction = params[:direction]
     @room = Room.where(x: params[:x].to_i, y: params[:y].to_i).first
+    @direction_long = Passage.normalize_direction(@direction).capitalize!
+    @dest = @room.get_destinations(@direction)
   end
 end
