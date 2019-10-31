6b818bd0ac66c9e4f4b1b62c1790d2d45bd2d24c
Terry Howe <terrylhowe@gmail.com>
2019-10-31 10:05:08 -0600

    Update gems

--- docker.sh
+++ docker.sh
@@ -1,2 +1,3 @@
-docker run --name terry-mysql -v /home/terry/mysql:/var/lib/mysql -e "MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}" -d --restart always mysql:5.7
-docker run --name rubymaze -e "MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}" -d --restart always rubymaze:latest
+export MYSQL_ROOT_PASSWORD=''
+docker run --name terry-mysql -v /home/terry/persistent/mysql:/var/lib/mysql -e "MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}" -d --restart always mysql:5.7
+# docker run --name rubymaze -e "MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}" -d --restart always rubymaze:latest
